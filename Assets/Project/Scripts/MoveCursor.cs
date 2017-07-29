6cdd9b9b12eb47e3b0f6712ee60503d933e255d1
Taro Omiya <japtar10101@gmail.com>
2017-07-29 11:04:48 -0400

    Creating a cursor pointing to a location on the course.

---
+++ Assets/Project/Scripts/MoveCursor.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveCursor : MonoBehaviour
+{
+    [SerializeField]
+    Camera raycastCamera;
+    [SerializeField]
+    LayerMask raycastLayer;
+    [SerializeField]
+    float raycastDistance = 100f;
+
+    public bool IsActive
+    {
+        get;
+        set;
+    }
+
+    Ray mouseRay;
+    RaycastHit mouseHit;
+
+	// Update is called once per frame
+	void Update ()
+    {
+        mouseRay = raycastCamera.ScreenPointToRay(Input.mousePosition);
+        IsActive = Physics.Raycast(mouseRay, out mouseHit, raycastDistance, raycastLayer.value);
+        if (IsActive == true)
+        {
+            transform.position = mouseHit.point;
+        }
+	}
+}
