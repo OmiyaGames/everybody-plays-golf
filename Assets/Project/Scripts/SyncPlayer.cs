95aae9936a2e971c07ab324d963d33a031af71a5
Taro Omiya <taro.omiya@omiyagames.com>
2017-07-30 09:57:40 -0400

    First attempt at querying directions from Database.

--- Assets/Project/Scripts/SyncPlayer.cs
+++ Assets/Project/Scripts/SyncPlayer.cs
@@ -13,9 +13,12 @@ namespace LudumDare39
 
         [SyncVar]
         Vector3 startingPosition;
+        [SerializeField]
+        float syncEverySeconds = 0.2f;
 
         MovePlayer player = null;
-        NetworkIdentity identity = null;
+        WaitForSeconds waitForSync = null;
+        double lastNetworkTime = 0;
 
         #region Properties
         public static SyncPlayer Instance
@@ -49,6 +52,18 @@ namespace LudumDare39
                 return player;
             }
         }
+
+        public WaitForSeconds WaitForSync
+        {
+            get
+            {
+                if(waitForSync == null)
+                {
+                    waitForSync = new WaitForSeconds(syncEverySeconds);
+                }
+                return waitForSync;
+            }
+        }
         #endregion
 
         public void QueueDirectionTowards(Vector3 position)
@@ -69,8 +84,114 @@ namespace LudumDare39
 
         void Start()
         {
+            lastNetworkTime = Network.time;
             instance = this;
+#if SERVER
+            StartCoroutine(QueryDatabase());
+#endif
         }
+
+#if SERVER
+        const char Divider = ',';
+        const int IdIndex = 0;
+        const int TimeIndex = IdIndex + 1;
+        const int XIndex = TimeIndex + 1;
+        const int ZIndex = XIndex + 1;
+
+        static readonly char[] Newline = new char[] { '\n' };
+
+        readonly List<ServerManager.Direction> queuedDirections = new List<ServerManager.Direction>();
+
+        void Update()
+        {
+            if(queuedDirections.Count > 0)
+            {
+                foreach(ServerManager.Direction direction in queuedDirections)
+                {
+                    Player.Move(direction.direction);
+                }
+                queuedDirections.Clear();
+            }
+        }
+
+        IEnumerator QueryDatabase()
+        {
+            yield return WaitForSync;
+
+            float lastSynced, syncTime;
+            while (gameObject.activeInHierarchy)
+            {
+                if (ServerManager.Instance)
+                {
+                    lastSynced = Time.time;
+
+                    yield return ServerManager.Instance.GetDirections(lastNetworkTime, ParseDirections);
+
+                    syncTime = (Time.time - lastSynced);
+                    if(syncTime < syncEverySeconds)
+                    {
+                        yield return new WaitForSeconds(syncEverySeconds - syncTime);
+                    }
+                }
+                else
+                {
+                    yield return WaitForSync;
+                }
+            }
+        }
+
+        void ParseDirections(bool status, string result)
+        {
+            if((status == true) && (string.IsNullOrEmpty(result) == false))
+            {
+                AppendDirections(result, queuedDirections);
+                lastNetworkTime = GetNewestTime(queuedDirections, lastNetworkTime); 
+            }
+        }
+
+        void AppendDirections(string info, ICollection<ServerManager.Direction> appendTo)
+        {
+            // Setup vars
+            int id;
+            float time, x, z;
+            Vector3 direction = Vector3.zero;
+            string[] cols = null;
+
+            // split by lines
+            string[] rows = info.Split(Newline, System.StringSplitOptions.RemoveEmptyEntries);
+            foreach (string row in rows)
+            {
+                // Split by comma
+                cols = info.Split(Divider);
+
+                // Try to parse everything
+                if ((cols.Length > ZIndex) &&
+                    int.TryParse(cols[IdIndex], out id) &&
+                    float.TryParse(cols[TimeIndex], out time) &&
+                    float.TryParse(cols[XIndex], out x) &&
+                    float.TryParse(cols[ZIndex], out z))
+                {
+                    // If successful, add a new direction
+                    direction.x = x;
+                    direction.z = z;
+                    appendTo.Add(new ServerManager.Direction(id, time, direction));
+                }
+            }
+        }
+
+        static double GetNewestTime(ICollection<ServerManager.Direction> appendTo, double defaultTime)
+        {
+            double newestTime = defaultTime;
+            foreach(ServerManager.Direction direction in appendTo)
+            {
+                if(direction.networkTime > newestTime)
+                {
+                    newestTime = direction.networkTime;
+                }
+            }
+            return newestTime;
+        }
+#endif
 
         void PrintStuff(bool success, string message)
         {
--- Database/GetDirections.php
+++ Database/GetDirections.php
@@ -2,10 +2,16 @@
     // Send variables for the MySQL database class.
     $database = mysql_connect('mysql_host', 'mysql_user', 'mysql_password') or die('Could not connect: ' . mysql_error());
     mysql_select_db('my_database') or die('Could not select database');
- 
+
     $query = "SELECT * FROM `directions` ORDER by `time` ASC LIMIT 10";
-    $result = mysql_query($query) or die('Query failed: ' . mysql_error());
+	if(isset($_GET['time']))
+	{
+        // Strings must be escaped to prevent SQL injection attack. 
+		$time = mysql_real_escape_string($_GET['time'], $db);
+		$query = "SELECT * FROM `directions` WHERE time>$time ORDER by `time` ASC LIMIT 10";
+	}
  
+    $result = mysql_query($query) or die('Query failed: ' . mysql_error());
     $num_results = mysql_num_rows($result);  
  
     for($i = 0; $i < $num_results; $i++)
