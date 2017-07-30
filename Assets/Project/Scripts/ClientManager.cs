5a63306aa5eb4379a76f06d5e11dd4c082d0df24
Taro Omiya <japtar10101@gmail.com>
2017-07-29 20:03:02 -0400

    Working on creating auto-servers and auto-clients.

---
+++ Assets/Project/Scripts/ClientManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+namespace LudumDare39
+{
+    public class ClientManager : MonoBehaviour
+    {
+        public const string DefaultServerUrl = "localhost";
+        public const string ServerUrlField = "ServerUrl";
+
+        public static string ServerUrl
+        {
+            get
+            {
+                return RemoteSettings.GetString(ServerUrlField, DefaultServerUrl);
+            }
+        }
+
+        NetworkClient myClient;
+
+        void Start()
+        {
+            myClient = new NetworkClient();
+            myClient.RegisterHandler(MsgType.Connect, OnConnected);
+            myClient.Connect(ServerUrl, ServerManager.Port);
+        }
+
+        private void OnConnected(NetworkMessage netMsg)
+        {
+            // FIXME: Do something!
+            Debug.Log("Connected to server");
+        }
+    }
+}
