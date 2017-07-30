5c95bd36f72deb9588dfc834fb69936e4df485a6
Taro Omiya <taro.omiya@omiyagames.com>
2017-07-29 20:03:02 -0400

    Working on creating auto-servers and auto-clients.

---
+++ Assets/Project/Scripts/ServerManager.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+namespace LudumDare39
+{
+    public class ServerManager : MonoBehaviour
+    {
+        public const int DefaultPort = 7777;
+        public const string PortField = "Port";
+
+        public static int Port
+        {
+            get
+            {
+                return RemoteSettings.GetInt(PortField, DefaultPort);
+            }
+        }
+
+        void Start()
+        {
+            //NetworkServer.set
+            NetworkServer.Listen(Port);
+            //NetworkServer.Configure()
+        }
+    }
+}
