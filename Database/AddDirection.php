67945123a016400d9db715ee3b03ac1400264e2d
Taro Omiya <japtar10101@gmail.com>
2017-07-29 23:43:35 -0400

    Adding cross domains and quering directions.

--- Database/AddDirection.php
+++ Database/AddDirection.php
@@ -1,5 +1,5 @@
 <?php 
-        $db = mysql_connect('omiyagamescom.fatcowmysql.com', 'mysql_user', 'mysql_password') or die('Could not connect: ' . mysql_error()); 
+        $db = mysql_connect('mysql_host', 'mysql_user', 'mysql_password') or die('Could not connect: ' . mysql_error()); 
         mysql_select_db('my_database') or die('Could not select database');
  
         // Strings must be escaped to prevent SQL injection attack. 
@@ -10,10 +10,10 @@
  
         $secretKey="mySecretKey"; # Change this value to match the value stored in the client javascript below 
 
-        $real_hash = md5($name . $score . $secretKey); 
+        $real_hash = md5($time . $x . $z . $secretKey); 
         if($real_hash == $hash) { 
             // Send variables for the MySQL database class. 
-            $query = "insert into scores values (NULL, '$name', '$score');"; 
+            $query = "insert into directions values (NULL, '$time', '$x', '$z');"; 
             $result = mysql_query($query) or die('Query failed: ' . mysql_error()); 
         } 
 ?>
