8b31bae2f099a6dec8c10c465b55c98f76bba487
Taro Omiya <taro.omiya@omiyagames.com>
2017-07-29 22:12:31 -0400

    Can't make the client abide to my rules.  Alas, time to move on.

--- Assets/Project/Scripts/ClientManager.cs
+++ Assets/Project/Scripts/ClientManager.cs
@@ -8,9 +8,23 @@ namespace LudumDare39
     [RequireComponent(typeof(NetworkManager))]
     public class ClientManager : IManager
     {
+        float time;
         void Start()
         {
+            time = Time.time;
             Manager.StartHost();
         }
+
+#if UNITY_EDITOR
+        private void Update()
+        {
+            if ((Time.time - time) > 3)
+            {
+                time = Time.time;
+                Manager.StopClient();
+                Manager.StartHost();
+            }
+        }
+#endif
     }
 }
