a77a1ccb263cca13404cda7adf1b2f00e78469f3
Taro Omiya <japtar10101@gmail.com>
2017-07-30 14:21:23 -0400

    Working on updating settings.

---
+++ Assets/Project/Scripts/Settings Modifier/v005-AddPower.cs
@@ -0,0 +1,71 @@
+﻿using OmiyaGames.Settings;
+
+namespace LudumDare39
+{
+    /// <summary>
+    /// Adds Options settings to <see cref="GameSettings"/>.
+    /// </summary>
+    public class AddPower : SettingsVersionGeneratorDecorator
+    {
+        public const ushort AppVersion = 5;
+        public const int DefaultMaxEnergy = 7;
+        public const int DefaultGameId = 0;
+        public const string MaxEnergyField = "MaxEnergy";
+
+        public override ushort Version
+        {
+            get
+            {
+                return AppVersion;
+            }
+        }
+
+        protected override string[] GetKeysToRemove()
+        {
+            // Do nothing!
+            return null;
+        }
+
+        public int MaxEnergy
+        {
+            get
+            {
+                return UnityEngine.RemoteSettings.GetInt(MaxEnergyField, DefaultMaxEnergy);
+            }
+        }
+
+        int GetEnergy(ISettingsRecorder settings, string key, int recordedVersion, int latestVersion, int defaultValue)
+        {
+            return settings.GetInt(key, MaxEnergy);
+        }
+
+        protected override IGenerator[] GetNewGenerators()
+        {
+            return new IGenerator[]
+            {
+                new StoredIntGenerator("Current Energy", DefaultMaxEnergy)
+                {
+                    TooltipDocumentation = new string[]
+                    {
+                        "Current energy on the dreaded energy meter."
+                    },
+                    Converter = GetEnergy
+                },
+                new StoredStringGenerator("Player Name", "")
+                {
+                    TooltipDocumentation = new string[]
+                    {
+                        "Stored name."
+                    }
+                },
+                new StoredIntGenerator("Last Game ID", DefaultGameId)
+                {
+                    TooltipDocumentation = new string[]
+                    {
+                        "Every time the golf ball makes it into the hole, the server game ID will go up by one.  This is the ID that the player last played.  If different from the server, proceed to recover energy."
+                    }
+                },
+            };
+        }
+    }
+}
